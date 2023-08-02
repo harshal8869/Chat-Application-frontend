4a84c6e0d255b1a09e49a98cbd303beec3962045
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-02 19:40:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [room, setRoom] = useState("");
    const [showChat, setShowChat] = useState(false);
   
-  const joinRoom = () => {
+   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
       setShowChat(true);
